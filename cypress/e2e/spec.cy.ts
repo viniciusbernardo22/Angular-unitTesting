58cfe41d7b565a7f58bb79945e3c7f48cd72513d
Vinícius <vini383@gmail.com>
2025-01-25 11:56:40 -0300

    cypress working

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,12 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running')
-  })
-})
+    cy.visit('/');
+
+    cy.get('input[name="title"]').type('Post title');
+    cy.get('textarea[name="body"]').type('Post body');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.post-title').should('contain', 'Post title');
+    cy.get('.post-body').should('contain', 'Post body');
+  });
+});
