c0e908d1eeda1f9640fda052b0da2353d15323f8
Vinícius <vini383@gmail.com>
2025-01-25 11:17:30 -0300

    add post card

--- src/app/post-card/post-card.component.spec.ts
+++ src/app/post-card/post-card.component.spec.ts
@@ -1,23 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostCardComponent } from './post-card.component';
+import { Post } from '../interfaces/post';
 
 describe('PostCardComponent', () => {
   let component: PostCardComponent;
   let fixture: ComponentFixture<PostCardComponent>;
-
+  let postMock: Post = {
+    title: 'postTitle',
+    body: 'postBody',
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PostCardComponent]
-    })
-    .compileComponents();
+      imports: [PostCardComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PostCardComponent);
+    fixture.componentRef.setInput('post', postMock);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display post title', () => {
+    const title = fixture.nativeElement.querySelector('.post-title');
+    expect(title.textContent).toContain(postMock.title);
+  });
+  it('should display post body', () => {
+    const title = fixture.nativeElement.querySelector('.post-body');
+    expect(title.textContent).toContain(postMock.body);
   });
 });
--- src/app/post-card/post-card.component.ts
+++ src/app/post-card/post-card.component.ts
@@ -1,13 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { Post } from '../interfaces/post';
 
 @Component({
   selector: 'app-post-card',
   imports: [],
   templateUrl: './post-card.component.html',
   styleUrl: './post-card.component.css',
-  standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  standalone: true
 })
 export class PostCardComponent {
-
+  readonly post = input.required<Post>();
 }
