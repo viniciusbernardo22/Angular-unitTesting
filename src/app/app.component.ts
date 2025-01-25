0d37a04bc0244a8bbd74d69beb7b3eaac620c367
Vinícius <vini383@gmail.com>
2025-01-25 11:17:42 -0300

    adjust appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Post } from './interfaces/post';
+import { PostCardComponent } from './post-card/post-card.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @Component({
   selector: 'app-root',
+  imports: [PostCardComponent, PostFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  
+  posts: Post[] = [
+    {
+      title: 'How to Learn Angular',
+      body: 'Angular is a powerful framework for building dynamic web applications. This guide will help you get started.',
+    },
+    {
+      title: 'Understanding TypeScript',
+      body: 'TypeScript is a typed superset of JavaScript that makes coding easier and more reliable. Dive in to learn the basics.',
+    },
+    {
+      title: 'CSS Tips for Beginners',
+      body: 'Want to make your websites look stunning? These CSS tips will help you design like a pro.',
+    },
+  ];
 }
